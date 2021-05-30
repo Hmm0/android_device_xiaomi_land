41f4bf65c7b9c35505e0840d589d3e86d11f61b9
Hmm0 <elprofessor936@gmail.com>
2021-05-30 09:35:41 +0000

    land: Specify Board Platform

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := hdpi
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msm8996
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
